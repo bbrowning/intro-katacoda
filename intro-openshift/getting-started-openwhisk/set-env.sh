b0aac2274e227da407683fd3ce03d75df977ff1d
Ben Browning <bbrownin@redhat.com>
2017-09-28 13:41:04 -0400

    Redirect stderr to /dev/null when polling for openwhisk readiness

--- intro-openshift/getting-started-openwhisk/set-env.sh
+++ intro-openshift/getting-started-openwhisk/set-env.sh
@@ -1,6 +1,6 @@
 ~/.launch.sh
 
 echo "Waiting for OpenWhisk to start... This may take a couple of moments"
-until $(oc logs controller-0 | grep "invoker status changed to 0 -> Healthy" &> /dev/null); do
+until $(oc logs controller-0 | grep "invoker status changed to 0 -> Healthy" 2&> /dev/null); do
   sleep 1
 done
