ddf3d463da023113260860aeefc2bb28a97eaf79
Ben Browning <bbrownin@redhat.com>
2017-09-28 14:15:01 -0400

    Show progress while waiting on OpenWhisk to deploy

--- intro-openshift/getting-started-openwhisk/set-env.sh
+++ intro-openshift/getting-started-openwhisk/set-env.sh
@@ -2,5 +2,8 @@
 
 echo "Waiting for OpenWhisk to start... This may take a couple of moments"
 until $(oc logs controller-0 2>&1 | grep "invoker status changed to 0 -> Healthy" &> /dev/null); do
-  sleep 1
+  sleep 5
+  echo -n "."
 done
+
+echo "OpenWhisk started."
